4b02f86577cbc136f230dc478d98dbb2b02de262
flashscope <flashscope@naver.com>
2015-01-03 23:05:13 +0900

    Init Project

---
+++ soran/src/org/nhnnext/soran/ui/session/SessionDetailActivity.java
@@ -0,0 +1,30 @@
+
+package org.nhnnext.soran.ui.session;
+
+import org.nhnnext.soran.R;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+
+public class SessionDetailActivity extends ActionBarActivity {
+
+	private ActionBar mActionbar;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_session_detail);
+		
+		mActionbar = getSupportActionBar();
+		mActionbar.setIcon(R.drawable.ic_soran_actionbar);
+		mActionbar.setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onSupportNavigateUp() {
+		finish();
+		return true;
+	}
+	
+}
